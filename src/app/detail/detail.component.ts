a0ed644b67b975eea6b87f6a6f9c090e5f6740fc
Joshua Morony <joshua.morony@gmail.com>
2023-10-15 13:05:27 +1030

    initial commit

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { TodoService } from '../shared/data-access/todo.service';
 
 @Component({
   standalone: true,
   selector: 'app-detail',
-  template: ` <h2>Detail</h2> `,
+  template: `
+    @if (todo(); as todo){
+    <h2>{{ todo.title }}</h2>
+    <p>{{ todo.description }}</p>
+    } @else {
+    <p>Could not find todo...</p>
+    }
+  `,
 })
-export default class DetailComponent { }
+export default class DetailComponent {
+  private route = inject(ActivatedRoute);
+  private todoService = inject(TodoService);
+
+  private paramMap = toSignal(this.route.paramMap);
+
+  todo = computed(() =>
+    this.todoService
+      .todos()
+      .find((todo) => todo.id === this.paramMap()?.get('id'))
+  );
+}
